3654b6e390085ef74ecba6c2bb3297e797406a87
Rifki Fahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2026-05-29 00:27:05 +0700

    Maps Frame

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -131,7 +131,7 @@ export default function Footer() {
               </a>
 
               {/* Map Embed Extension */}
-              <div className="mt-5 rounded-2xl overflow-hidden border border-white/10 h-48 bg-white/5 relative">
+              <div className="mt-5 rounded-2xl overflow-hidden border border-white/10 h-48 bg-white/5 relative group/map">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.444243642354!2d104.7570413!3d-2.9742442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b7700684f09d5%3A0x6a0a099a800b462c!2sTriple%20A%20Coffee!5e0!3m2!1sid!2sid!4v1716900000000!5m2!1sid!2sid"
                   width="100%"
@@ -142,6 +142,14 @@ export default function Footer() {
                   referrerPolicy="no-referrer-when-downgrade"
                   className="grayscale hover:grayscale-0 transition-all duration-500 opacity-80 hover:opacity-100"
                 ></iframe>
+                {/* Clickable Overlay to direct to user's specific link */}
+                <a
+                  href="https://maps.app.goo.gl/MnJjgMbhDxxd2bZF6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute inset-0 z-10 cursor-alias"
+                  title="Buka di Google Maps"
+                ></a>
               </div>
             </div>
           </FadeInUp>
